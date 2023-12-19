f66e43a077ed4384c8b58e1f00854026f9e22253
Wesley lopes <111934185+WesleyLopes12@users.noreply.github.com>
2023-12-19 10:42:54 -0300

    Add files via upload

---
+++ AloMundo.java
@@ -0,0 +1,24 @@
+public class AloMundo{
+
+public static void main(String[] args) {
+
+    int n1 = 10;
+    int n2 = 20;
+    int n3 = 30;
+    int res = n1+n2+n3;
+    
+    String nome = "Wesley Lopes"; 
+    System.out.println("CFB Cursos\n");
+    System.out.println("Curso de Java");
+    System.out.printf("canal:%s%nAno:%d%n", "CFB Cursos", 2023);
+    System.out.printf("Valor da variavel num:%d%nNome:%s",res,nome);
+
+}
+
+
+
+
+
+
+
+}
