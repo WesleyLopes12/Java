7dc10e05f2d4499d4b60cb35de485a6d22501669
Wesley <westleylopes4@gmail.com>
2024-03-02 18:55:29 -0300

    novos arquivos

--- Exercicios/src/Operadores/Atribuicao.java
+++ Exercicios/src/Operadores/Atribuicao.java
@@ -1,10 +1,47 @@
+<<<<<<< HEAD
+=======
+<<<<<<<< HEAD:Exercicios/src/Atribuicao.java
+>>>>>>> 787978a (novos arquivos)
 package Operadores;
 
 public class Atribuicao {
 
 	public static void main(String[] args) {
 		
+<<<<<<< HEAD
 		System.out.println("Wesley Lopes Show");
 	}
 
 }
+=======
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		
+		c += b; //c = c + b;
+		c -= a; //c = c - a;
+		c *= b; // c = c *b;
+		c /= a;// c = c / a;
+		
+		c++; // c = c +1;
+		
+		System.out.println(c);
+		
+		c %= 2; // c = c % 2;
+		System.out.println(c);
+	}
+
+}
+========
+package Operadores;
+
+public class Atribuicao {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+>>>>>>>> 787978a (novos arquivos):Exercicios/src/Operadores/Atribuicao.java
+>>>>>>> 787978a (novos arquivos)
